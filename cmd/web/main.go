3bc2e4225b6dc9530e2644794ec5b25f7d975ec4
damirkhamidullin <khamidullin.2019@mail.ru>
2025-01-23 12:43:49 +0300

    First run!

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,7 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"html/template"
+	"log"
+	"net/http"
+)
+
+func RegPage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseGlob("./ui/html/*"))
+	err := tmpl.ExecuteTemplate(w, "registration.html", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
+
+func MainPage(w http.ResponseWriter, r *http.Request) {
+	tmpl := template.Must(template.ParseGlob("./ui/html/*"))
+	err := tmpl.ExecuteTemplate(w, "index.html", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+}
 
 func main() {
-	fmt.Println("Easy to start, hard to finish!")
+	mux := http.NewServeMux()
+	mux.HandleFunc("/reg", RegPage)
+	mux.HandleFunc("/main", MainPage)
+
+	fmt.Println("Сервер запущен! Адрес: 127.0.0.1:8082")
+	http.ListenAndServe("127.0.0.1:8082", mux)
 }
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/RinatZaynet/CouchFilmCritic
+
+go 1.22.2
